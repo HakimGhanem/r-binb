6708d5ba0618467a1330585497046d7c5bd32980
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2017-08-21 08:10:47 +0000

    Ajout model Room

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,29 @@ has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x10
                   :default_url => "/assets/default_image.png"
 validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
+# Model Room
+
+has_many:rooms  #Un user peut avoir une ou plusieurs annonces de logement
+
+validates :home_type, presence: true  #la variable type d’habitation sera nécessaire pour la création d’une annonce  
+           validates :room_type, presence: true
+
+
+           validates :accommodate, presence: true
+
+
+           validates :bed_room, presence: true
+
+
+           validates :bath_room, presence: true
+
+
+           validates :listing_name, presence: true, length: {maximum: 75} # 75 caractères max  
+           validates :summary, presence: true, length: {maximum: 600}
+
+
+           validates :address, presence: true
+
+
+           validates :price, numericality: { only_integer: true, greater_than: 5 } #seulement des nombres entiers supérieurs à 5
 end
