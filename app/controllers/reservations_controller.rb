e8bde83cf05904dd797b722a3cb9ca0fc94b5384
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2017-08-23 14:42:33 +0000

    Gestion dates Ajax..

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,35 @@
 class ReservationsController < ApplicationController
 # encoding: utf-8
     before_action :authenticate_user! #Condition, il faut que l'utilisateur soit connecté pur pouvoir reserver.
+    
+    def preload
+        
+        room = Room.find(params[:room_id])
+        today = Date.today
+        reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+        
+        render json: reservations
+    end
+    
+    def preview
+        start_date = Date.parse(params[:start_date])
+        end_date = Date.parse(params[:end_date])
+        
+        output = {
+            conflict: is_conflict(start_date, end_date)
+        }
+        
+        render json: output
+    end
+
+    private def is_conflict(start_date, end_date)
+        room = Room.find(params[:room_id])
+ 
+        check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+        check.size > 0? true : false
+    end
+    
+    
     def create
         @reservation = current_user.reservations.create(reservation_params)
         redirect_to @reservation.room, notice: "Votre réservation a bien été enregistrée"
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   end
   resources :photos  
 
+
+get '/preload' => 'reservations#preload'
+get '/preview' => 'reservations#preview'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
