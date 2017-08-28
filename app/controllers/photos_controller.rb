4ac04a89fb3a350ce9ec3939928fbbf578a7152c
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2017-08-28 07:49:22 +0000

    Réglage bug JS suppression photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,15 @@
 class PhotosController < ApplicationController
+    
     def destroy
         
-        @photo = Photo.find(params[:id])
+        @photo = Photo.find(params[:id]) # Correspond à l'id de la photo cocernée
         room = @photo.room
         
         @photo.destroy
         
-        @photos = Photo.where(room_id: room_id)
+        @photos = Photo.where(room_id: room.id) #Réactualise la liste des photos de ce logement
         
-        respond_to :js
+        respond_to :js #Suppression immédiate
     end
     
 end
